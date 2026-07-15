a260df6b30b75b671f68f63f1e9b649419b11b9c
Michael Montgomery <michaelmontgomery@MW-GPI-F0Q6WQJ7.local>
2026-07-14 22:11:50 -0400

    content and styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import projects from './data/projects';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faSms } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
+
+
+
+
+  const [professionalMode, setProfessionalMode] = useState(true);
+  const [projectsList, setProjectsList] = useState(projects);
+
+
+  const goToProject = (link) => {
+    window.open(link, '_blank');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <><div className="home-wrapper">
+      <h1>Michael Montgomery</h1>
+      <p className='title'>Senior Software Engineer at Johns Hopkins University</p>
+      <div className="headshot-wrapper"></div>
+      <p className='bio'>Michael joined Johns Hopkins University as a Senior Software Engineer in October 2024. Prior to this, he worked with companies like Boeing, Universal Orlando, Disney and L3Harris among others. At Johns Hopkins University, Michael has done work to support the <a href="https://cde.govex.jhu.edu/">City Data Explorer</a> (a unique tool, offering an interactive look into highly granular data related to larger cities in the United States), was the lead engineer on the <a href="https://govex.jhu.edu/ai-capacity-quiz/">AI Capacity Quiz project</a> and currently works to drive the University’s various AI initiatives forward.</p>
+      <p className='bio'>Please feel free to reach out. I would love to hear from you!</p>
+
     </div>
+      <div className='projects-wrapper'>
+        <h3>Projects<span>{projectsList.length}</span></h3>
+        <input type="text" placeholder="Search projects..." onChange={(e) => {
+          const searchTerm = e.target.value.toLowerCase();
+          const filteredProjects = projects.filter(project =>
+            project.title.toLowerCase().includes(searchTerm) ||
+            project.description.toLowerCase().includes(searchTerm) ||
+            project.company.toLowerCase().includes(searchTerm) ||
+            project.tags.some(tag => tag.toLowerCase().includes(searchTerm))
+          );
+          setProjectsList(filteredProjects);
+        }} />
+        {projectsList.map((project, index) => (
+          <div key={index} className='project-card' onClick={() => goToProject(project.link)}>
+            <h4>{project.title} <span className='company'>{project.company}</span></h4>
+            <p>{project.description}</p>
+            {/* <a href={project.link} target="_blank" rel="noopener noreferrer">View Project</a> */}
+            {/* <p>Company: {project.company}</p> */}
+            <div className='tags'>
+              {project.tags.map((tag, tagIndex) => (
+                <span key={tagIndex} className='tag'>{tag}</span>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="connect-wrapper">
+        <h3>Connect</h3>
+        <select onChange={(e) => setProfessionalMode(e.target.value === 'professional')}>
+          <option value="professional">Professional</option>
+          <option value="casual">Personal</option>
+        </select>
+        {/* <p className='connect-text'>{professionalMode ? "Feel free to connect with me on LinkedIn, send me a text or check out my GitHub profile." : "Feel free to connect with me on LinkedIn, send me a text or check out my GitHub profile."}</p> */}
+        <div className="connect-links">
+
+          {
+            professionalMode && (
+              <a href="https://www.linkedin.com/in/meanstackmichael/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
+            )
+          }
+          {!professionalMode && (
+            <a href="sms:+13214661405" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faSms} /></a>
+
+          )}
+
+          {!professionalMode && (
+            <a href="https://www.instagram.com/foiemardigras/" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} /></a>
+          )}
+
+          {professionalMode && (
+            <a href="https://github.com/Michael-Montgomery" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} /></a>
+          )}
+
+
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/data/projects.js
@@ -0,0 +1,41 @@
+let projects = [
+  {
+    title: "AI Capacity Quiz",
+    description: "A quiz to help organizations assess their AI readiness and capacity.",
+    link: "https://govex.jhu.edu/ai-capacity-quiz/",
+    company: "Johns Hopkins University",
+    tags: ["fullstack", "react", "nodejs", "express"]
+  },
+  {
+    title: "LB Tile Co.",
+    description: "A company specializing in high-quality tiles for residential and commercial projects.",
+    link: "https://www.lbtile.com",
+    company: "LB Tile Co.",
+    tags: ["fullstack", "react", "nodejs", "express"]
+
+  },
+  {
+    title: "City Data Explorer",
+    description: "An interactive tool for exploring granular data related to larger cities in the United States.",
+    link: "https://cde.govex.jhu.edu/",
+    company: "Johns Hopkins University",
+    tags: ["frontend", "react", "Next.js"]
+  },
+  {
+    title: "Meetup API",
+    description: "A NodeJS/Express API to consume the Smartorder API and provide a simplified interface for the Orlando Informer team and customers to use in their applications.",
+    link: "https://github.com/Michael-Montgomery/OI-Tickets",
+    company: "Orlando Informer",
+    tags: ["backend", "nodejs", "express"]
+  },
+  {
+    title: "Smartorder API",
+    description: "A custom, RESTful (Node/Express) service used for ticketing and park catalog integrations",
+    link: "https://qacorpapi.ucdp.net/smartorder/help/index.html",
+    company: "Universal Orlando",
+    tags: ["backend", "nodejs", "express"]
+  }
+  
+];
+
+export default projects;
